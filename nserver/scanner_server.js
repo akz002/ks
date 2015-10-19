e41b51e1f73f4792ad9099e23c8a4090507935d4
Kevin Zhang <kzhang01@gmail.com>
2015-10-18 21:23:28 -0500

    use logger

--- nserver/scanner_server.js
+++ nserver/scanner_server.js
@@ -2,33 +2,37 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
- 
+
 var scanner = io.of('/scanner'); 
 
 var BTDevices = ["a4d856039ebf", "ee443390fa9d", "fb738cf43b8a"];
 
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+
 scanner.on('connection', function(socket) {
  
-    console.log('Scanner Connected');
+    logger.debug('Scanner Connected');
     
 //    socket.on('deviceData', function(msg) {
+/*
     socket.on('uuid', function(uuid) {
-        console.log('uuid: ' + uuid);
+        logger.debug('uuid: ' + uuid);
         //recived message from scanner
         //do some processing here
     });
-
+*/
     socket.on('rssi', function(rssi) { 
-        console.log('rssi: ' + rssi); 
+        logger.debug('rssi: ' + rssi); 
         //recived message from scanner 
         //do some processing here
     }); 
   
     socket.on('disconnect', function() {
-        console.log('Scanner Disconnected');
+        logger.debug('Scanner Disconnected');
     });
 });
  
 http.listen(3000, function() {
-    console.log('listening on *:3000');
+    logger.debug('listening on *:3000');
 });
